82727e5223743bd5ff107b37c85e131fcd79ca51
Калина Ненова <kalina.nenova@paisii.info>
2024-03-17 10:35:39 +0200

    First commit

---
+++ BookandFilmShop/Controllers/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookandFilmShop.Controllers
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ BookandFilmShop/Data/BookandFilmShopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookandFilmShop.Controllers;
+
+namespace BookandFilmShop.Data
+{
+    public class BookandFilmShopContext : DbContext
+    {
+        public BookandFilmShopContext (DbContextOptions<BookandFilmShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookandFilmShop.Controllers.Book> Book { get; set; } = default!;
+    }
+}
