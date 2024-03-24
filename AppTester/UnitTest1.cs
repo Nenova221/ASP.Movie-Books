2543196826a41392b6fcc0feb7b1a91a5ef16de7
P0povaI <123860481+P0povaI@users.noreply.github.com>
2024-03-24 12:50:04 +0200

    Add files via upload

--- AppTester/UnitTest1.cs
+++ AppTester/UnitTest1.cs
@@ -1,47 +1,108 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
-using BookandFilmShop.Controllers;
 using BookandFilmShop.Data;
 using BookandFilmShop.Models;
-using Humanizer.Localisation;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Moq;
-using NUnit.Framework.Constraints;
-using System.IO;
 
-namespace AppTester
+namespace BookandFilmShop.Controllers
 {
-    public class Tests
+    [TestFixture]
+    public class BookandFilmShopContextTests
     {
-        [SetUp]
-        public void Setup()
+        private DbContextOptions<BookandFilmShopContext> GetDbContextOptions()
         {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<BookandFilmShopContext>()
+                .UseInMemoryDatabase("TestDatabase")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
         }
 
         [Test]
-        public async Task Index_ReturnViewWithResult()
+        public async Task Index_ReturnsViewWithListOfShoes()
         {
-            var mockContext = new Mock<BookandFilmShopContext>();
-            var movies = new List<Movies>
+            var options = GetDbContextOptions();
+            using (var context = new BookandFilmShopContext(options))
             {
-               new Movies  { Id=1, Title="Аргайл: Супершпионин", Genre="Екшън/Комедия", Director="Матю Вон", Description="Филма си заслужава"},
-               new Movies { Id = 1, Title = "Аргайл: Супершпионин", Genre = "Екшън/Комедия", Director = "Матю Вон", Description ="Филма си заслужава" }
-            }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Movies>>();
-            mockSet.As<IQueryable<Movies>>().Setup(m => m.Provider).Returns(movies.Provider);
-            mockSet.As<IQueryable<Movies>>().Setup(m => m.Expression).Returns(movies.Expression);
-            mockSet.As<IQueryable<Movies>>().Setup(m => m.ElementType).Returns(movies.ElementType);
-            mockSet.As<IQueryable<Movies>>().Setup(m => m.GetEnumerator()).Returns(movies.GetEnumerator());
-
-            mockContext.Setup(m=>m.Movies).Returns(mockSet.Object);
-            var controller = new MoviesController(mockContext.Object);
-            var result = await controller.Index();
-
-            Assert.That(result,Is.TypeOf<ViewResult>());
-            var viewResult = result as ViewResult;
-            var model = viewResult.Model as IEnumerable<Movies>;
-            Assert.That(model.Count, Is.EqualTo(1));
+                context.Movies.Add(new Movies { Id = 1, Title = "Scream", Genre = "horror", Director = "Matt Bettinelli-Olpin", Description = "Scream is an American slasher franchise that includes six films, a television series, merchandise, and games."});
+                context.SaveChanges();
+            }
+            using (var context = new BookandFilmShopContext(options))
+            {
+                var controller = new MoviesController(context);
+                var result = await controller.Index();
+                // Assert
+                Assert.IsInstanceOf<ViewResult>(result);
+            }
+        }
+        [Test]
+        public async Task Delete_ReturnsRedirectToActionResult()
+        {
+            var options = GetDbContextOptions();
+            using (var context = new BookandFilmShopContext(options))
+            {
+                context.Movies.Add(new Movies { Id = 1, Title = "Scream", Genre = "horror", Director = "Matt Bettinelli-Olpin", Description = "Scream is an American slasher franchise that includes six films, a television series, merchandise, and games." });
+                context.SaveChanges();
+            }
+            using (var context = new BookandFilmShopContext(options))
+            {
+                var controller = new MoviesController(context);
+                var result = await controller.DeleteConfirmed(1);
+                // Assert
+                Assert.IsInstanceOf<RedirectToActionResult>(result);
+            }
+        }
+        [Test]
+        public async Task Edit_ReturnsRedirectToActrionResult()
+        {
+            var options = GetDbContextOptions();
+            using (var context = new BookandFilmShopContext(options))
+            {
+                context.Movies.Add(new Movies { Id = 1, Title = "Scream", Genre = "horror", Director = "Matt Bettinelli-Olpin", Description = "Scream is an American slasher franchise that includes six films, a television series, merchandise, and games." });
+                context.SaveChanges();
+            }
+            using (var context = new BookandFilmShopContext(options))
+            {
+                var controller = new MoviesController(context);
+                var result = await controller.Edit(1);
+                // Assert
+                Assert.IsInstanceOf<ViewResult>(result);
+            }
+        }
+        [Test]
+        public async Task Create_ReturnsRedirectToActrionResult()
+        {
+            var options = GetDbContextOptions();
+            var shoe = new  Movies { Id = 1, Title = "Scream", Genre = "horror", Director = "Matt Bettinelli-Olpin", Description = "Scream is an American slasher franchise that includes six films, a television series, merchandise, and games." };
+            using (var context = new BookandFilmShopContext(options))
+            {
+                var controller = new MoviesController(context);
+                var result = await controller.Create(shoe);
+                // Assert
+                Assert.IsInstanceOf<RedirectToActionResult>(result);
+            }
+        }
+        [Test]
+        public async Task Details_ReturnsRedirectToActrionResult()
+        {
+            var options = GetDbContextOptions();
+            using (var context = new BookandFilmShopContext(options))
+            {
+                context.Movies.Add(new Movies { Id = 1, Title = "Scream", Genre = "horror", Director = "Matt Bettinelli-Olpin", Description = "Scream is an American slasher franchise that includes six films, a television series, merchandise, and games." });
+                context.SaveChanges();
+            }
+            using (var context = new BookandFilmShopContext(options))
+            {
+                var controller = new MoviesController(context);
+                var result = await controller.Details(1);
+                // Assert
+                Assert.IsInstanceOf<ViewResult>(result);
+            }
         }
     }
 }
